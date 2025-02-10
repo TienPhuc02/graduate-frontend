b33b2d195ca0067b68a0349764ea73f897eeec4e
TienPhuc02 <dtp2352002@gmail.com>
2025-02-10 20:35:53 +0700

    hero section

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,12 +1,14 @@
 import Benefits from '@/components/feature/homepage/Benefits'
 import RecommendCourse from '@/components/feature/homepage/RecommendCourse'
 import HeaderHomePage from '@/components/layout/HeaderHomePage'
+import Hero from '../../components/feature/homepage/Hero'
 
 const HomePage = () => {
   return (
     <>
       <HeaderHomePage />
       <div className='container mx-auto'>
+        <Hero />
         <Benefits />
         <RecommendCourse />
       </div>
