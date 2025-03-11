c3211c3eaa23c1b8838acdd8e5530086d8fe764f
TienPhuc02 <dtp2352002@gmail.com>
2025-03-11 16:54:31 +0700

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from 'tailwindcss'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+// import path from 'path'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,7 +17,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      // '@': path.resolve(__dirname, './src')
     }
   }
 })
